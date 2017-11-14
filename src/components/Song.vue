af0922cd54b66b9dd9ea002e6e3e496d929ca49c
Kyle Hall <hallkd47@gmail.com>
2017-11-13 22:10:58 -0600

    Uses a border color, not background color, to signal completed status

--- src/components/Song.vue
+++ src/components/Song.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="song">
         <div v-if="song.learned">
-            <h3 class="ba bg-light-green bw2 mw5 center pa5">{{ song.title }}</h3>
+            <h3 class="ba green-border bw2 mw5 center pa5">{{ song.title }}</h3>
         </div>
         <div v-else>
-            <h3 class="ba bg-light-red bw2 mw5 center pa5">{{ song.title }}</h3>
+            <h3 class="ba red-border bw2 mw5 center pa5">{{ song.title }}</h3>
         </div>
     </div>
     
